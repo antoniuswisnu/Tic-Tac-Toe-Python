8e83cc84e3598268134d5869bca997a2f01cefa0
antoniuswisnu <88194426+antoniuswisnu@users.noreply.github.com>
2022-01-15 05:58:08 +0700

    Update Tic Tac Toe.py

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -1,14 +1,19 @@
 board = [' ' for x in range(10)]
+
 def insertLetter(letter, pos):
     board[pos] = letter
+    
 def spaceIsFree(pos):
     return board[pos] == " "
+
 def printBoard(board):
+    print("---------")
     print(" " + board[1] + "| " + board[2] + "| " + board[3])
     print("---------")
     print(" " + board[4] + "| " + board[5] + "| " + board[6])
     print("---------")
     print(" " + board[7] + "| " + board[8] + "| " + board[9])
+    
 def isWinner(bo, le):
     return ((bo[7] == le and bo[8] == le and bo[9] == le) or 
             (bo[4] == le and bo[5] == le and bo[6] == le) or 
@@ -18,10 +23,11 @@ def isWinner(bo, le):
             (bo[3] == le and bo[6] == le and bo[9] == le) or 
             (bo[1] == le and bo[5] == le and bo[9] == le) or  
             (bo[3] == le and bo[5] == le and bo[7] == le))
+
 def playMove():
     run = True
     while run:
-        move = input("Please select a position to place an 'X' (1-9): ")
+        move = input("x (1-9): ")
         try:
             move = int(move)
             if move > 0 and move < 10 :
@@ -29,11 +35,25 @@ def playMove():
                     run = False
                     insertLetter("x", move)
                 else:
-                    print("Sorry this space is occupied")
-            else:
-                print("Please type the number within the range")
+                    print("MAAF TEMPAT TERSEBUT TIDAK TERSEDIA")
         except:
-            print("Please type a number. ")
+            print("MASUKAN NOMOR DENGAN BENAR")
+            
+def playMove2():
+    run = True
+    while run:
+        move2 = input("o (1-9): ")
+        try:
+            move2 = int(move2)
+            if move2 > 0 and move2 < 10 :
+                if spaceIsFree(move2):
+                    run = False
+                    insertLetter("o", move2)
+                else:
+                    print("MAAF TEMPAT TERSEBUT TIDAK TERSEDIA")
+        except:
+            print("MASUKAN NOMOR DENGAN BENAR")
+
 def compMove():
     possibleMoves = [x for x, letter in enumerate(board) if letter == ' ' and x != 0]
     move = 0
@@ -61,38 +81,66 @@ def compMove():
     if len(edgesOpen) > 0:
         move = selectRandom(edgesOpen)
     return move
+
 def selectRandom(li):
     import random
     ln = len(li)
     r = random.randrange(0, ln)
     return li[r]
+            
 def isBoardFull(board):
     if board.count(" ") > 1:
         return False
     else:
         return True
-def main():
-    print("welcome tic tac toe")
+
+def main1():
+    print("Selamat Datang Di Tic Tac Toe")
     printBoard(board)
     while not(isBoardFull(board)):
         if not(isWinner(board, "o")):
             playMove()
             printBoard(board)
         else:
-            print("Sorry, O's won this time! ")
+            print("SELAMAT O MENANG!")
             break
         if not(isWinner(board, "x")):
             move  = compMove()
             if move == 0:
-                print("Tie game")
+                print("PERMAINAN IMBANG!")
             else:
                 insertLetter("o", move)
-                print("Computer placed an 'o' in position", move, ":")
+                print("Computer memasang 'o' di kotak", move, ":")
                 printBoard(board)
         else:
-            print("X's won this time! Good job")
+            print("SELAMAT X MENANG!")
             break
     if isBoardFull(board):
-        print("Tie game")
+        print("PERMAINAN IMBANG!")
 
-main()
+def main2():
+    print("Selamat Datang Di Tic Tac Toe")
+    printBoard(board)
+    while not(isBoardFull(board)):
+        if not(isWinner(board, "o")):
+            playMove()
+            printBoard(board)
+        else:
+            print("SELAMAT O MENANG!")
+            break
+        if not(isWinner(board, "x")):
+            playMove2()
+            printBoard(board)
+        else:
+            print("SELAMAT X MENANG!")
+            break
+    if isBoardFull(board):
+        print("PERMAINAN IMBANG!")
+
+print("----PILIH MODE PERMAINAN----")
+print("1 Pemain / 2 Pemain ")
+x = int(input("(1/2)= "))
+if x == 1:
+    main1()
+elif x == 2:
+    main2()
